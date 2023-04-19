220f2a9758ab334655bd90d05dcab9b983d5952d
Luan Panno <luanpanno@gmail.com>
2023-04-19 02:35:28 -0300

    Reorganize code into modules

--- learn-rust-by-building-real-applications/http-server/src/main.rs
+++ learn-rust-by-building-real-applications/http-server/src/main.rs
@@ -1,38 +1,11 @@
+use server::Server;
+
+mod http;
+mod server;
+
 fn main() {
     let address = "127.0.0.1:8080";
     let server = Server::new(address.to_string());
 
     server.run();
-}
-
-struct Server {
-    addr: String,
-}
-
-impl Server {
-    fn new(addr: String) -> Self {
-        Self { addr }
-    }
-
-    fn run(self) {
-        println!("Listening on {}", self.addr);
-    }
-}
-
-struct Request {
-    path: String,
-    query_string: Option<String>,
-    method: Method,
-}
-
-enum Method {
-    GET,
-    POST,
-    PUT,
-    DELETE,
-    PATCH,
-    HEAD,
-    CONNECT,
-    OPTIONS,
-    TRACE,
 }
