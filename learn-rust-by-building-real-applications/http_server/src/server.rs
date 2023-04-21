0ecdcf76af35665851e5ad4cc4f2588fd8b2f79c
Luan Panno <luanpanno@gmail.com>
2023-04-21 00:02:08 -0300

    Match expression

--- learn-rust-by-building-real-applications/http_server/src/server.rs
+++ learn-rust-by-building-real-applications/http_server/src/server.rs
@@ -1,3 +1,5 @@
+use std::net::TcpListener;
+
 pub struct Server {
     addr: String,
 }
@@ -9,5 +11,15 @@ impl Server {
 
     pub fn run(self) {
         println!("Listening on {}", self.addr);
+
+        let listener = TcpListener::bind(&self.addr).unwrap();
+
+        loop {
+            match listener.accept() {
+                Ok(_) => println!("OK"),
+
+                Err(err) => println!("Error: {}", err),
+            }
+        }
     }
 }
